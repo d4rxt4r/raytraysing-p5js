9cfb40c31e8252d012c1d3a661bb45e8e3ef4e81
d4rxt4r <rakow47@gmail.com>
2024-04-20 18:29:40 +0300

    Implemented samples stratification

--- classes/Camera.js
+++ classes/Camera.js
@@ -1,4 +1,4 @@
-import { deg2rad } from '../utils/math.js';
+import { int, deg2rad } from '../utils/math.js';
 import { Vector, vec3 } from '../utils/vector.js';
 import { color } from '../utils/image.js';
 import HitRecord from './HitRecord.js';
@@ -10,10 +10,28 @@ const { add, sub, mul, cross, scale } = Vector;
 const DT = new Interval(0.001, Infinity);
 
 export default class Camera {
+   imageWidth;
+   imageHeight;
+   maxDepth;
+
+   #center;
+   #pixelSamplesScale;
+   #sqrtSpp;
+   #invSqrtSpp;
+   #viewportHeight;
+   #viewportWidth;
+   #w;
+   #u;
+   #v;
+   #pixelDeltaU;
+   #pixelDeltaV;
+   #pixel00Loc;
+   #defocusDiskU;
+   #defocusDiskV;
+
    constructor(iw = 100, ih = 100, settings = {}) {
       this.imageWidth = iw;
       this.imageHeight = ih;
-
       this.maxDepth = settings.maxDepth || 20;
       this.spp = settings.spp || 30;
       this.defocusAngle = settings.defocusAngle || 0;
@@ -26,55 +44,55 @@ export default class Camera {
    }
 
    init() {
-      this.pixelSamplesScale = 1 / this.spp;
-      this.center = this.lookFrom.copy();
+      this.#sqrtSpp = int(Math.sqrt(this.spp));
+      this.#pixelSamplesScale = 1 / (this.#sqrtSpp * this.#sqrtSpp);
+      this.#invSqrtSpp = 1 / this.#sqrtSpp;
 
-      const theta = deg2rad(this.vFov);
-      const h = Math.tan(theta / 2);
+      this.#center = this.lookFrom.copy();
 
-      this.viewportHeight = 2 * h * this.focusDist;
-      this.viewportWidth = this.viewportHeight * (this.imageWidth / this.imageHeight);
+      this.#viewportHeight = 2 * Math.tan(deg2rad(this.vFov) / 2) * this.focusDist;
+      this.#viewportWidth = this.#viewportHeight * (this.imageWidth / this.imageHeight);
 
-      this.w = sub(this.lookFrom, this.lookAt).normalize();
-      this.u = cross(this.vUp, this.w).normalize();
-      this.v = cross(this.w, this.u);
+      this.#w = sub(this.lookFrom, this.lookAt).normalize();
+      this.#u = cross(this.vUp, this.#w).normalize();
+      this.#v = cross(this.#w, this.#u);
 
-      const viewportU = scale(this.u, this.viewportWidth);
-      const viewportV = scale(this.v, -this.viewportHeight);
+      const viewportU = scale(this.#u, this.#viewportWidth);
+      const viewportV = scale(this.#v, -this.#viewportHeight);
 
-      this._pixelDeltaU = scale(viewportU, 1 / this.imageWidth);
-      this._pixelDeltaV = scale(viewportV, 1 / this.imageHeight);
+      this.#pixelDeltaU = scale(viewportU, 1 / this.imageWidth);
+      this.#pixelDeltaV = scale(viewportV, 1 / this.imageHeight);
 
-      const viewportUL = sub(this.center, scale(this.w, this.focusDist))
+      const viewportUL = sub(this.#center, scale(this.#w, this.focusDist))
          .sub(viewportU.scale(1 / 2))
          .sub(viewportV.scale(1 / 2));
 
-      this._pixel00Loc = add(this._pixelDeltaU, this._pixelDeltaV).add(viewportUL);
+      this.#pixel00Loc = add(this.#pixelDeltaU, this.#pixelDeltaV).add(viewportUL);
 
       const defocusRadius = this.focusDist * Math.tan(deg2rad(this.defocusAngle / 2));
-      this._defocusDiskU = scale(this.u, defocusRadius);
-      this._defocusDiskV = scale(this.v, defocusRadius);
+      this.#defocusDiskU = scale(this.#u, defocusRadius);
+      this.#defocusDiskV = scale(this.#v, defocusRadius);
    }
 
-   #sampleSquare() {
-      return vec3(Math.random() - 0.5, Math.random() - 0.5, 0);
+   #sampleSquareStratified(si, sj) {
+      const px = (si + Math.random()) * this.#invSqrtSpp - 0.5;
+      const py = (sj + Math.random()) * this.#invSqrtSpp - 0.5;
+
+      return vec3(px, py, 0);
    }
 
    #defocusDiskSample() {
       const p = Vector.randomNormDisk();
-      return add(this.center, scale(this._defocusDiskU, p.x)).add(scale(this._defocusDiskV, p.y));
+      return add(this.#center, scale(this.#defocusDiskU, p.x)).add(scale(this.#defocusDiskV, p.y));
    }
 
-   #getRay(i, j) {
-      const offset = this.#sampleSquare();
-      const pixelSample = add(this._pixel00Loc, scale(this._pixelDeltaU, i + offset.x)).add(
-         scale(this._pixelDeltaV, j + offset.y)
+   #getRay(i, j, si, sj) {
+      const offset = this.#sampleSquareStratified(si, sj);
+      const pixelSample = add(this.#pixel00Loc, scale(this.#pixelDeltaU, i + offset.x)).add(
+         scale(this.#pixelDeltaV, j + offset.y)
       );
-      const rayOrigin = this.defocusAngle <= 0 ? this.center.copy() : this.#defocusDiskSample();
-      const rayDirection = sub(pixelSample, rayOrigin);
-      const rayTime = Math.random();
-
-      return new Ray(rayOrigin, rayDirection, rayTime);
+      const rayOrigin = this.defocusAngle <= 0 ? this.#center.copy() : this.#defocusDiskSample();
+      return new Ray(rayOrigin, sub(pixelSample, rayOrigin), Math.random());
    }
 
    #getRayColor(scene, ray, depth) {
@@ -100,16 +118,17 @@ export default class Camera {
    }
 
    render(scene, x, y) {
-      if (!this._pixel00Loc) {
+      if (!this.#pixel00Loc) {
          this.init();
       }
 
       const pixelColor = color(0, 0, 0);
-      for (let sample = 0; sample < this.spp; sample++) {
-         const ray = this.#getRay(x, y);
-         pixelColor.add(this.#getRayColor(scene, ray, this.maxDepth));
+      for (let sj = 0; sj < this.#sqrtSpp; sj++) {
+         for (let si = 0; si < this.#sqrtSpp; si++) {
+            pixelColor.add(this.#getRayColor(scene, this.#getRay(x, y, si, sj), this.maxDepth));
+         }
       }
 
-      return pixelColor.scale(this.pixelSamplesScale);
+      return pixelColor.scale(this.#pixelSamplesScale);
    }
 }
